3fba7c00d5e9b374a7e63f2befdb76da72607cda
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-09 16:49:15 +0200

    boldly forward, we continue with the exercises

---
+++ 01.FirstStepInCoding-Lab/07.ProjectsCreation/Program.cs
@@ -0,0 +1,6 @@
+﻿string nameArchitect = Console.ReadLine();
+int countProjects = int.Parse(Console.ReadLine());
+int timeForOneProject = 3;
+int timeForAllProjects = countProjects * timeForOneProject;
+
+Console.WriteLine($"The architect {nameArchitect} will need {timeForAllProjects} hours to complete {countProjects} project/s.");
