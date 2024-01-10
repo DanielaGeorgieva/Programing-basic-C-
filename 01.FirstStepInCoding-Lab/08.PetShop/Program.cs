ed0e09b715218713fa9db666ff9ea05c0e6e935f
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-10 11:35:33 +0200

    I solved 2 more tasks.

---
+++ 01.FirstStepInCoding-Lab/08.PetShop/Program.cs
@@ -0,0 +1,7 @@
+﻿int countFoodDog = int.Parse(Console.ReadLine());
+int countFoodCat = int.Parse(Console.ReadLine());
+
+
+double totalSum = (countFoodDog * 2.50) + (countFoodCat * 4.00);
+
+Console.WriteLine($"{totalSum} lv.");
---
+++ 01.FirstStepInCoding-Lab/09.YardGreening/Program.cs
@@ -0,0 +1,8 @@
+﻿double allYard = double.Parse(Console.ReadLine());
+
+double sumAllYard = allYard * 7.61;
+double discount = sumAllYard * 0.18;
+double sumWithDiscount = sumAllYard - discount;
+
+Console.WriteLine($"The final price is: {sumWithDiscount} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
