b1eda884407fca074358b6e74a8a3c067fbfbf1d
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-12 15:25:45 +0200

    More tasks and exercises

---
+++ FirstStepsInCoding-Exercise/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,16 @@
+﻿int packagesPen = int.Parse(Console.ReadLine());
+int packagesMarkers = int.Parse(Console.ReadLine());
+int litres = int.Parse(Console.ReadLine());
+int discount = int.Parse(Console.ReadLine());
+
+double pricePen = 5.80;
+double priceMarkers = 7.20;
+double priceLiter = 1.20;
+
+double neededSum = ((packagesPen * pricePen) + (packagesMarkers * priceMarkers) + (litres * priceLiter));
+
+double totalSum = neededSum - (neededSum * discount / 100);
+
+
+
+Console.WriteLine(totalSum);
---
+++ FirstStepsInCoding-Exercise/06.Repainting/Program.cs
@@ -0,0 +1,14 @@
+﻿int qualityNylon = int.Parse(Console.ReadLine());
+int qualityPaint = int.Parse(Console.ReadLine());
+int qualityТhinner = int.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+
+double priceNylon = 1.50;
+double pricePaint = 14.50;
+double priceThinner = 5.00;
+double priceBag = 0.40;
+double neededSum = ((qualityNylon + 2) * priceNylon) + ((qualityPaint + (qualityPaint * 0.10)) * pricePaint) + (qualityТhinner * priceThinner) + priceBag;
+double priceForWorkers = (neededSum * 0.30) * hours;
+double totalSum = neededSum + priceForWorkers;
+
+Console.WriteLine(totalSum);
