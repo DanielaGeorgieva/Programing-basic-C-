c33ff7722a086f98b180454224612b4504c39edb
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-12 10:34:13 +0200

    "New tasks"

--- 01.FirstStepInCoding-Lab/09.YardGreening/obj/Debug/net6.0/09.YardGreening.AssemblyInfo.cs
+++ 01.FirstStepInCoding-Lab/09.YardGreening/obj/Debug/net6.0/09.YardGreening.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("09.YardGreening")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0e09b715218713fa9db666ff9ea05c0e6e935f")]
 [assembly: System.Reflection.AssemblyProductAttribute("09.YardGreening")]
 [assembly: System.Reflection.AssemblyTitleAttribute("09.YardGreening")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
