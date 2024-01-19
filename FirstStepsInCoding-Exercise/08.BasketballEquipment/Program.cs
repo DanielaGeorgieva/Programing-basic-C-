a408da7a9cf3a8828e1bb335cf393740b2d725c8
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-19 14:07:15 +0200

    More two solved tasks

---
+++ FirstStepsInCoding-Exercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,10 @@
+﻿int sumPerYear = int.Parse(Console.ReadLine());
+
+double shoes = sumPerYear - (sumPerYear * 0.40);
+double equipment = shoes - (shoes * 0.20);
+double ball = equipment / 4;
+double accessories = ball / 5;
+
+double allSum = sumPerYear + shoes + equipment + ball + accessories;
+
+Console.WriteLine(allSum);
