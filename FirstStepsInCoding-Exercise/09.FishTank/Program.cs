e176a1b3e9a30bdb8cec8b66a75479afda570e94
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-19 14:37:06 +0200

    The last one of this lesson

---
+++ FirstStepsInCoding-Exercise/09.FishTank/Program.cs
@@ -0,0 +1,9 @@
+﻿int lenght = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int hight = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+int volume = lenght * width * hight;
+double litres = volume * 0.001;
+double allLitres = litres * (1 - percent / 100);
+Console.WriteLine(allLitres);
