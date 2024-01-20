72a9d39f57131eba3450246c1d869f8f87de43dc
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-20 13:13:15 +0200

    I've finished this lesson

--- ConditionalStatements-Lab/04.PasswordGuess/obj/Debug/net6.0/04.PasswordGuess.AssemblyInfo.cs
+++ ConditionalStatements-Lab/04.PasswordGuess/obj/Debug/net6.0/04.PasswordGuess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04.PasswordGuess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e176a1b3e9a30bdb8cec8b66a75479afda570e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("04.PasswordGuess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04.PasswordGuess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConditionalStatements-Lab/07.AreaOfFigures/Program.cs
@@ -0,0 +1,30 @@
+﻿string figure = Console.ReadLine();
+
+double area = 0;
+
+if (figure == "square")
+{
+    double num = double.Parse(Console.ReadLine());
+    area = num * num;
+    Console.WriteLine($" {area:f3}");
+}
+else if (figure == "rectangle")
+{
+    double numA = double.Parse(Console.ReadLine());
+    double numB = double.Parse(Console.ReadLine());
+    area = numA * numB;
+    Console.WriteLine($" {area:f3}");
+}
+else if (figure == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    area = Math.PI * radius * radius;
+    Console.WriteLine($" {area:f3}");
+}
+else if (figure == "triangle")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    double hight = double.Parse(Console.ReadLine());
+    area = sideA * hight / 2;
+        Console.WriteLine($" {area:f3}");
+}
