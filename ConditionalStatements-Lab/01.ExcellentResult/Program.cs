9a31b2b53e03c3ab9fc99506df0801929858c58b
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-20 11:45:44 +0200

    The new lesson has begun!

---
+++ ConditionalStatements-Lab/01.ExcellentResult/Program.cs
@@ -0,0 +1,5 @@
+﻿double result = double.Parse(Console.ReadLine());
+if  (result >= 5.5)
+{
+    Console.WriteLine("Excellent!");
+}
---
+++ ConditionalStatements-Lab/03.EvenOrOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Design;
+
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+
+else
+{
+    Console.WriteLine("odd");
+}
+
