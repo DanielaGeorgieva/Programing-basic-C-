8ee5b90d93ad102a896d70b3b0633d768dfc7359
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-23 12:46:30 +0200

    DG

---
+++ 02.ConditionalStatements-Exercise/07.Shopping/Program.cs
@@ -0,0 +1,26 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int videoCardCount = int.Parse(Console.ReadLine());
+int processorsCount = int.Parse(Console.ReadLine());
+int ramMemoryCount = int.Parse(Console.ReadLine());
+
+double priceCards = videoCardCount * 250;
+double priceProcessor = priceCards * 0.35;
+double priceRam = priceCards * 0.10;
+
+double neededSum = (priceCards + (processorsCount * priceProcessor) + (ramMemoryCount * priceRam));
+
+if (videoCardCount > processorsCount)
+{
+    neededSum *= 0.85;
+}
+
+if (budget >= neededSum)
+{
+    double totalSum = Math.Abs(neededSum - budget);
+    Console.WriteLine($"You have {totalSum:F2} leva left!");
+}
+else
+{
+    double totalSum = Math.Abs(neededSum - budget);
+    Console.WriteLine($"Not enough money! You need {totalSum:F2} leva more!");
+}
---
+++ 02.ConditionalStatements-Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string nameSerial = Console.ReadLine();
+int longTimeSeries = int.Parse(Console.ReadLine());
+int timeRest = int.Parse(Console.ReadLine());
+
+double lunch = timeRest / 8.0;
+double rest = timeRest / 4.0;
+
+double neededTime = longTimeSeries + lunch + rest;
+
+double difference = Math.Abs(neededTime - timeRest);
+
+if (neededTime <= timeRest)
+{
+    
+    Console.WriteLine($"You have enough time to watch {nameSerial} and left with {Math.Ceiling(difference)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {nameSerial}, you need {Math.Ceiling(difference)} more minutes.");
+}
