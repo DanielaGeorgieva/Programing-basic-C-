07f6b97c7b0dc56473c4db0a6ff3ab9d60358876
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-24 12:51:38 +0200

    DG is working

---
+++ 03.ConditionalStatementsAdvanced-Lab/04.PersonalTitles/Program.cs
@@ -0,0 +1,20 @@
+﻿double age = double.Parse(Console.ReadLine());
+string sex = Console.ReadLine();
+
+if (sex == "m")
+if (age >= 16)
+{
+        Console.WriteLine("Mr.");
+    }
+if (sex == "m" && age < 16)
+    {
+        Console.WriteLine("Master");
+    }
+else if (sex == "f" && age >= 16)
+{
+    Console.WriteLine("Ms.");
+}
+else if (sex == "f" && age < 16)
+{
+    Console.WriteLine("Miss");
+}
