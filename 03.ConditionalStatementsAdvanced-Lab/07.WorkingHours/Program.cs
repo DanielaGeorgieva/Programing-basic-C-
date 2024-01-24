b7ce6fcadf178f9e89ec60df1543af888e74f009
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-24 16:10:02 +0200

    Very nice code

---
+++ 03.ConditionalStatementsAdvanced-Lab/07.WorkingHours/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+int hour = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+if (hour >= 10 && hour <= 18 && day != "Sunday")
+{
+    switch (day)
+    {
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+        case "Saturday":
+            break;
+    }
+    Console.WriteLine("open");
+}
+else
+{
+    Console.WriteLine("closed");
+}
+
---
+++ 03.ConditionalStatementsAdvanced-Lab/08.CinemaTicket/Program.cs
@@ -0,0 +1,18 @@
+﻿string day = Console.ReadLine();
+// Monday	Tuesday	Wednesday	Thursday	Friday	Saturday	Sunday
+//12         12     14          14           12      16          16
+int price = 0;
+
+if (day == "Monday" || day == "Tuesday" || day == "Friday")
+{
+    price = 12;
+}
+else if (day == "Wednesday" || day == "Thursday")
+{
+    price = 14;
+}
+else if (day == "Saturday" || day == "Sunday")
+{
+    price = 16;
+}
+Console.WriteLine(price);
