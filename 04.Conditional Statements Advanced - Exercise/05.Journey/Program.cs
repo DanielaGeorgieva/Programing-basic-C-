e580069a059757329fecdfb6ee22661ea1c0262f
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-01-30 14:27:17 +0200

    DG

--- 04.Conditional Statements Advanced - Exercise/05.Journey/Program.cs
+++ 04.Conditional Statements Advanced - Exercise/05.Journey/Program.cs
@@ -1,4 +1,47 @@
 ﻿double budget = double.Parse(Console.ReadLine());
 string season = Console.ReadLine();
+string destination = "";
+string typeRest = "";
 
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+
+    switch (season)
+    {
+        case "summer":
+            budget *= 0.30;
+            typeRest = "Camp";
+            break;
+        case "winter":
+            budget *= 0.70;
+            typeRest = "Hotel";
+            break;
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+
+    switch (season)
+    {
+        case "summer":
+            budget *= 0.40;
+            typeRest = "Camp";
+            break;
+        case "winter":
+            budget *= 0.80;
+            typeRest = "Hotel";
+            break;
+    }
+}
+else if (budget > 1000)
+
+{
+    destination = "Europe";
+    budget *= 0.90;
+    typeRest = "Hotel";
+}
+    Console.WriteLine($"Somewhere in {destination}");
+    Console.WriteLine($"{typeRest} - {budget:f2}");
 
---
+++ 04.Conditional Statements Advanced - Exercise/06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+char symbol = char.Parse(Console.ReadLine());
+
+double calculation = 0.0;
+switch (symbol)
+{
+    case '+':
+        calculation = numOne + numTwo;
+        if (calculation % 2 == 0)
+
+            Console.WriteLine($"{numOne} + {numTwo} = {calculation} - even");
+        else
+            Console.WriteLine($"{numOne} + {numTwo} = {calculation} - odd");
+        break;
+    case '-':
+        calculation = numOne - numTwo;
+        if (calculation % 2 == 0)
+
+            Console.WriteLine($"{numOne} - {numTwo} = {calculation} - even");
+        else
+            Console.WriteLine($"{numOne} - {numTwo} = {calculation} - odd");
+        break;
+    case '*':
+        calculation = numOne * numTwo;
+        if (calculation % 2 == 0)
+
+            Console.WriteLine($"{numOne} * {numTwo} = {calculation} - even");
+        else
+            Console.WriteLine($"{numOne} * {numTwo} = {calculation} - odd");
+        break;
+
+    case '/':
+        if (numTwo != 0)
+        {
+            calculation = (double)numOne / numTwo;
+            Console.WriteLine($"{numOne} / {numTwo} = {calculation:f2}");
+        }
+        else
+            Console.WriteLine($"Cannot divide {numOne} by zero");
+        break;
+
+    case '%':
+        if (numTwo != 0)
+        {
+            calculation = (double)numOne % numTwo;
+            Console.WriteLine($"{numOne} % {numTwo} = {calculation}");
+        }
+
+        else
+            Console.WriteLine($"Cannot divide {numOne} by zero");
+        break;
+}
