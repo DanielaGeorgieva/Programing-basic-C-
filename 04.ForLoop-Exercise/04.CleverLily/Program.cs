41f2954fcd7836a276e52cf77f1f42c54e646cac
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-02-08 15:39:19 +0200

    It is hard exercise

---
+++ 04.ForLoop-Exercise/04.CleverLily/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Channels;
+
+int ageLily = int.Parse(Console.ReadLine());
+double priceWashingMachine = double.Parse(Console.ReadLine());
+int toyPrice = int.Parse(Console.ReadLine());
+
+int money = 0;
+
+for (int i = 1; i <= ageLily; i++)
+{
+    if (i % 2 == 0)
+        money += i * 5 - 1;
+    else
+        money += toyPrice;
+}
+if (money >= priceWashingMachine)
+
+    Console.WriteLine($"Yes! {money - priceWashingMachine:f2}");
+else
+
+    Console.WriteLine($"No! {priceWashingMachine - money:f2}" );
