3795aa4176584a8f0b19a695cb7243c161a762d0
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-02-08 12:06:53 +0200

    This was difficult

--- 04.ForLoop-Lab/09.LeftAndRightSum/Program.cs
+++ 04.ForLoop-Lab/09.LeftAndRightSum/Program.cs
@@ -5,8 +5,19 @@ int right = 0;
 for (int i = 0; i < n; i++)
 {
     int currentNum = int.Parse(Console.ReadLine());
+    left += currentNum;
 }
 for (int i = 0; i < n; i++)
 {
     int currentNum = int.Parse(Console.ReadLine());
+    right += currentNum;
+}
+if (left == right)
+{
+    Console.WriteLine($"Yes, sum = {left} ");
+}
+else
+{
+    int difference = Math.Abs(left - right);
+    Console.WriteLine($"No, diff = {difference}");
 }
---
+++ 04.ForLoop-Lab/10.OddEvenSum/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int oddSum = 0;
+int evenSum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+    
+    if (i % 2 == 0)
+    {
+        evenSum += currentNum;
+    }
+    else
+    {
+        oddSum += currentNum;
+    }
+}
+if (oddSum == evenSum)
+{
+    Console.WriteLine($"Yes ");
+    Console.WriteLine($"Sum = {oddSum} ");
+}
+else
+{
+    int difference = Math.Abs(oddSum - evenSum);
+    Console.WriteLine($"No ");
+    Console.WriteLine($"Diff = {difference}");
+}
