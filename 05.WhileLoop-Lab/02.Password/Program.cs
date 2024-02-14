ceb8d3a08a7733ecd642fc79767ad7007d91a63d
Dani <84021722+DanielaGeorgieva@users.noreply.github.com>
2024-02-14 14:17:39 +0200

    I'm still here learning about While loop

---
+++ 05.WhileLoop-Lab/02.Password/Program.cs
@@ -0,0 +1,12 @@
+﻿string username = Console.ReadLine();
+string password = Console.ReadLine();
+
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == password)
+    {
+        Console.WriteLine($"Welcome {username}!");
+        break;
+    }
+}
---
+++ 05.WhileLoop-Lab/03.SumNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while(sum < num)
+{
+    int newNum = int.Parse(Console.ReadLine());
+    sum += newNum;
+}
+
+Console.WriteLine(sum);
